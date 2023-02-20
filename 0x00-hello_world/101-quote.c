c01600c100b49e51d7919760e47acf530d35c49d
Eric Nkomejimana <enkomejimana322@gmail.com>
2023-02-20 13:02:18 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that prints a line to the standard error
- * Return: 1 (Success)
+ * main - - Entry the correct point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, str, sizeof(str) - 1);
+return (1);
 }
+
