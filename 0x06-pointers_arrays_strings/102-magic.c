e49ff854c7f2bee770da8e79ca3fe18b9f41e55d
Eric Nkomejimana <enkomejimana322@gmail.com>
2023-03-02 00:34:23 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * This program demonstrates a pointer trick that allows the value
+ * of a variable to be modified indirectly. The goal is to modify
+ * the value of a[2] without directly assigning to it or using the
+ * variable 'a'.
+ *
+ * Return: Always 0
+ */
+
+
 int main(void)
 {
 int n;
@@ -8,17 +20,8 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
 *(p + 5) = 98;
   /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
-
